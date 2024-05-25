7607f2a30d10a8072046bc5f7bd081a11fb7cfd9
lymeansreang <1220.ly.meansreang@rupp.edu.kh>
2024-05-25 17:12:50 +0700

    Created Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,13 @@ import 'package:tamago_store/utils/theme/theme.dart';
 import 'app.dart';
 
 void main() {
+
+  // TODO: Add Widget Binding
+  // TODO: Init Local Storage
+  // TODO: Await Native Splash
+  // TODO: Initialize Firebase
+  // TODO: Initialize Authentication
+
   runApp(const App());
 }
 
