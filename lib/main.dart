4a3be915308c5c485791f46544ea2d3f7412b3cc
lymeansreang <1220.ly.meansreang@rupp.edu.kh>
2024-06-08 18:00:51 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
 
   // TODO: Add Widget Binding
   // TODO: Init Local Storage
   // TODO: Await Native Splash
   // TODO: Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  // .then((FirebaseApp value) => Get.put(AuthenticationRepository()))
   // TODO: Initialize Authentication
 
   runApp(const App());
